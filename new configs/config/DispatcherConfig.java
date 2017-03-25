bf8340fc97e7b159aa8f1f0af249db4488fc1b64
Charchit <charchit26@gmail.com>
2017-03-26 02:14:47 +0530

    java based config added

---
+++ new configs/config/DispatcherConfig.java
@@ -0,0 +1,21 @@
+package com.infy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "com.infy" })
+public class DispatcherConfig {
+	@Bean(name="viewResolver")
+	public InternalResourceViewResolver resolver(){
+		InternalResourceViewResolver r=new InternalResourceViewResolver();
+		r.setPrefix("/WEB-INF/jsp/");
+		r.setSuffix(".jsp");
+		return r;
+	}
+	
+}
